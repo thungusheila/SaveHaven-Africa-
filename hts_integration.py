a3055d067c8bb4b9e3cf6d34b70046cda36a5069
Fillo001 <153615610+Fillo001@users.noreply.github.com>
2025-09-14 23:04:02 +0100

    Create hts_integration.py

---
+++ hts_integration.py
@@ -0,0 +1,28 @@
+from hedera import TokenCreateTransaction, TokenMintTransaction  
+from hedera_client import client  
+
+def create_token(name, symbol, initial_supply):  
+    tx = (  
+        TokenCreateTransaction()  
+        .setTokenName(name)  
+        .setTokenSymbol(symbol)  
+        .setDecimals(0)  
+        .setInitialSupply(initial_supply)  
+        .setTreasuryAccountId(client.operatorAccountId)  
+        .setAdminKey(client.operatorPublicKey)  
+        .freezeWith(client)  
+    )  
+    signed = tx.sign(client.operatorPrivateKey)  
+    receipt = signed.execute(client).getReceipt(client)  
+    return receipt.tokenId.toString()  
+
+def mint_token(token_id, amount):  
+    tx = (  
+        TokenMintTransaction()  
+        .setTokenId(token_id)  
+        .setAmount(amount)  
+        .freezeWith(client)  
+    )  
+    signed = tx.sign(client.operatorPrivateKey)  
+    receipt = signed.execute(client).getReceipt(client)  
+    return receipt.status.toString()
